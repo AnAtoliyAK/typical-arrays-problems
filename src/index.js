d04c108d670dae762fde04cd40c98ecd34eed8c2
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-02-29 21:35:29 +0200

    feat: implement min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if ((Array.isArray(array)) == false) { return 0; }
+    else if (array.length == 0) { return 0; }
+    else {
+        let array1 = array.sort((a, b) => a - b);
+        return array1[0];
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if ((Array.isArray(array)) == false) { return 0; }
+    else if (array.length == 0) { return 0; }
+    else {
+        let array1 = array.sort((a, b) => b - a);
+        return array1[0];
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if ((Array.isArray(array)) == false) { return 0; }
+    else if (array.length == 0) { return 0; }
+    else {
+        let sum = 0;
+        for (let item of array) {
+            sum += item;
+        }
+        let result = (sum / array.length);
+        return result;
+    }
 }
